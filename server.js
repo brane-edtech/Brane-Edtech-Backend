a245852a5f26605277dbb7397ed19e298d75a50b
brane-edtech <138586189+brane-edtech@users.noreply.github.com>
2023-07-11 11:09:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,26 +3,25 @@ const cors=require("cors");
 const express=require("express");
 const app=express();
 const header = require("./routes/homescreen/header");
-const child1 = require("./routes/homescreen/child1");
+const banner = require("./routes/homescreen/banner");
 const ourfeatures = require("./routes/homescreen/ourfeatures");
 const ourprojects = require("./routes/homescreen/ourprojects");
 const leadersvoice = require("./routes/homescreen/leadersvoice");
 const parentsvoice = require("./routes/homescreen/parentsvoice");
-const languages = require("./routes/homescreen/languages");
-const forbes = require("./routes/homescreen/forbes");
+const newsletter = require("./routes/homescreen/newsletter");
 const footer = require("./routes/homescreen/footer");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/homepage_header", header);
-app.use("/homepage_child1", child1);
+app.use("/homepage_banner", banner);
 app.use("/homepage_ourfeatures", ourfeatures);
 app.use("/homepage_ourprojects", ourprojects);
 app.use("/homepage_leadersvoice", leadersvoice);
 app.use("/homepage_parentsvoice", parentsvoice);
 app.use("/homepage_languages", languages);
-app.use("/homepage_forbes", forbes);
+app.use("/homepage_newsletter", newsletter);
 app.use("/homepage_footer", footer);
 
 app.listen(process.env.PORT || 8080, () => {
