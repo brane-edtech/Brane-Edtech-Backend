8d111713af96d0706c02df0d093256978298e670
brane-edtech <138586189+brane-edtech@users.noreply.github.com>
2023-07-11 11:11:18 +0530

    Update and rename forbes.js to newsletter.js

--- routes/homescreen/newsletter.js
+++ routes/homescreen/newsletter.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const braneClient = require("../../client/client");
-const forbes = express.Router();
+const newsletter = express.Router();
 
-forbes.get("/", async (req, res)=>{
+newsletter.get("/", async (req, res)=>{
     try{
         await braneClient.connect()
-        let data = await braneClient.db("brane").collection("homepage_forbes").find().toArray();
+        let data = await braneClient.db("brane").collection("homepage_newsletter").find().toArray();
         if(data.length === 0 || data.includes(null)){
             res.status(404).json({error:"Data Not Found"});
         }
@@ -16,4 +16,4 @@ forbes.get("/", async (req, res)=>{
     }
 })
 
-module.exports = forbes;
+module.exports = newsletter;
